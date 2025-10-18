180ec1918a1ca97099fb8ff77ac0d1283c9a66ac
msgill1337 <mehdeep.work@gmail.com>
2025-10-18 18:47:30 -0400

    removed sensitive files - added AKS resource code

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
         source = "hashicorp/azurerm"
-        version = "~> 3.0.2"
+        version = "~> 4.0"
     }
   }
 
@@ -12,11 +12,12 @@ terraform {
 provider "azurerm" {
   features {
   }
+  subscription_id = "860e5706-a4d0-4c7c-89c9-4bd4aaeee4ad"
 }
 
 resource "azurerm_resource_group" "tf-aks-rg" {
-  name = var.resource_group_config.name
-  location = var.resource_group_config.location
+  name = var.vnet_resource_group_config.name
+  location = var.vnet_resource_group_config.location
 }
 
 module "networking" {
@@ -24,9 +25,21 @@ module "networking" {
   vnet_configuration = var.vnet_configuration
   subnet_configuration = var.subnet_configuration
   nsg_rules = var.nsg_rules
-  resource_group_config = var.resource_group_config
+  vnet_resource_group_config = var.vnet_resource_group_config
   tags = var.tags
   nsg_configuration = var.nsg_configuration
+}
+
+module "aks" {
+  source = "./modules/aks"
+  aks_node_pool_subnet_id = module.networking.subnet_ids["prod-aks-subnet01"]
+  user_node_pool_subnet_id = module.networking.subnet_ids["prod-user-subnet01"]
+  kubernetes_cluster_configuration = var.kubernetes_cluster_configuration
+  tags = var.tags
+  kubernetes_node_pool_configuration = var.kubernetes_node_pool_configuration
+  aks_resource_group_config = var.aks_resource_group_config
+  aks_log_analytics_workspace_config = var.aks_log_analytics_workspace_config
+  depends_on = [module.networking]
 }
 
 output "VNET_ID" {
--- variables.tf
+++ variables.tf
@@ -22,7 +22,7 @@ variable "subnet_configuration" {
   description = "List of subnets with name and address space"
 }
 
-variable "resource_group_config" {
+variable "vnet_resource_group_config" {
   type = object({
     name = string
     location = string
@@ -56,4 +56,57 @@ variable "nsg_rules" {
         source_address_prefix = string
         destination_address_prefix = string
     }))  
+}
+
+variable "kubernetes_cluster_configuration" {
+    type = object({
+      name = string
+      dns_prefix = string
+      kubernetes_version = string
+      azure_active_directory_role_based_access_control = object({
+        azure_rbac_enabled = bool
+        tenant_id = string
+      })
+      default_node_pool = object({
+        name = string
+        node_count = number
+        vm_size = string
+        auto_scaling_enabled = bool
+        type = string
+        min_count = number
+        max_count = number
+      })
+
+      identity = object({
+        type = string
+      })
+    })
+}
+
+variable "kubernetes_node_pool_configuration" {
+  type = object({
+    name = string
+    vm_size = string
+    node_count = number
+    auto_scaling_enabled = bool
+    max_count = number
+    min_count = number
+    zones = list(string)
+  })
+
+}
+
+variable "aks_resource_group_config" {
+  type = object({
+    name = string
+    location = string
+  })
+}
+
+variable "aks_log_analytics_workspace_config" {
+  type = object({
+    name = string
+    sku = string
+    retention_in_days = number
+  })
 }
